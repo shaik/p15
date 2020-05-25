2a3fe2063b547c2f6e1a4fadfe56e2410d463a3f
shaik <shai.kfir@gmail.com>
2020-05-25 07:53:14 +0300

    ind

--- src/board.ts
+++ src/board.ts
@@ -7,25 +7,27 @@ export class Board {
     this.init();
   }
 
-  err =  {
-    'index_out_of_bound': 1,
-    'index_too_far': 2,
-    'cant_move_blank': 3
-  }
+  err = {
+    "index_out_of_bound": 1,
+    "index_too_far": 2,
+    "cant_move_blank": 3,
+  };
+  
   private init() {
-    let a: number[] = [...Array(this.size ** 2).keys()]
+    let a: number[] = [...Array(this.size ** 2).keys()];
     this.fold(a);
   }
 
   private fold(a: number[]) {
     this._board = [];
-    while (a.length > 0)
+    while (a.length > 0) {
       this._board.push(a.splice(0, this.size));
+    }
   }
 
   shuffle() {
-    let a: number[] = [...Array(this.size ** 2).keys()]
-    for (let i =this.size ** 2 - 1; i > 0; i--) {
+    let a: number[] = [...Array(this.size ** 2).keys()];
+    for (let i = this.size ** 2 - 1; i > 0; i--) {
       const j = Math.floor(Math.random() * (i + 1));
       [a[i], a[j]] = [a[j], a[i]];
     }
@@ -44,19 +46,21 @@ export class Board {
     return this._size;
   }
 
-  move(x: number, y: number): number{
-    if (x<0 || y<0 || x>=this.size || y>=this.size) return this.err.index_out_of_bound;
+  move(x: number, y: number): number {
+    if (x < 0 || y < 0 || x >= this.size || y >= this.size) {
+      return this.err.index_out_of_bound;
+    }
     if (this.board[y][x] === 0) return this.err.cant_move_blank;
-    const [px, py]  = this.getBlankPos();
-    if (Math.abs(px - x) + Math.abs(py -y) != 1) return this.err.index_too_far
-    
+    const [px, py] = this.getBlankPos();
+    if (Math.abs(px - x) + Math.abs(py - y) != 1) return this.err.index_too_far;
+
     this.board[py][px] = this.board[y][x];
     this.board[y][x] = 0;
     return 0;
   }
 
   private getBlankPos(): [number, number] {
-    const p: number = this.board.flat().findIndex(x => x == 0);
+    const p: number = this.board.flat().findIndex((x) => x == 0);
     const zy: number = Math.floor(p / this.size);
     const zx: number = p % this.size;
     return [zx, zy];
@@ -65,7 +69,7 @@ export class Board {
   toString(): string {
     let s: string = "\n";
     for (let i: number = 0; i < this.size; i++) {
-      s += this._board[i].join(', ') + "\n";
+      s += this._board[i].join(", ") + "\n";
     }
     return s;
   }
