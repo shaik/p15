908a49cdecddbcb5321ab462911c6d22ea676851
shaik <shai.kfir@gmail.com>
2020-05-30 03:24:57 +0300

    port should be a number

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,8 @@
 import {app, startServer} from "./server.ts"
 
 const env = Deno.env.toObject();
-const PORT = env.PORT || 4000;
-const HOST = env.HOST || "127.0.0.1";
+const PORT: number = parseInt(env.PORT) || 4000;
+const HOST: string = env.HOST || "127.0.0.1";
 
 console.log(`Listening on port ${PORT}...`);
 console.log(" root " + `${Deno.cwd()}/static`);
